4f205c8f8b13b81a7fdd6465374f5a4132b9ecde
Spedwards <minecraftsped@gmail.com>
2014-05-06 21:22:28 +1000

    Get-Browser

---
+++ getbrowser/assets/demo.js
@@ -0,0 +1,27 @@
+function getBrowser() {
+	if (/Google Inc./.test(navigator.vendor)) {
+		return 'Google Chrome';
+	} else if (/Safari/.test(navigator.userAgent)) {
+		return 'Safari';
+	} else if (/Opera Software ASA/.test(navigator.vendor)) {
+		return 'Opera';
+	} else if (/Microsoft Internet Explorer/.test(navigator.appName)) {
+		return 'Microsoft Internet Explorer';
+	} else if (/Firefox/.test(navigator.userAgent) && !/PaleMoon/.test(navigator.userAgent) && !/SeaMonkey/.test(navigator.userAgent)) {
+		return 'Mozilla Firefox';
+	} else if (/K-Meleon/.test(navigator.vendor)) {
+		return 'K-Meleon';
+	} else if (/Maxthon Asia Ltd./.test(navigator.vendor)) {
+		return 'Maxthon';
+	} else if (/PaleMoon/.test(navigator.userAgent)) {
+		return 'Pale Moon';
+	} else if (/SeaMonkey/.test(navigator.userAgent)) {
+		return 'SeaMonkey';
+	} else if (/Arora/.test(navigator.userAgent)) {
+		return 'Arora';
+	} else {
+		return 'an unknown browser';
+	}
+}
+
+document.getElementById('browser').innerHTML = getBrowser();
