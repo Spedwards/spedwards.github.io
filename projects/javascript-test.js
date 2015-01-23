c030c40ab55a89947f54d014be471409ff157a1b
Liam <Spedwards@users.noreply.github.com>
2015-01-23 23:02:14 +1100

    Create javascript-test.js

---
+++ projects/javascript-test.js
@@ -0,0 +1,17 @@
+var charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890',
+	charset_length = charset.length;
+
+function recurse(width, pos, base) {
+	for (var i = 0; i < charset_length; i++) {
+		if (pos < width - 1) {
+			recurse(width, pos+1, base + charset[i]);
+		}
+		console.log(base + charset[i]);
+	}
+}
+
+function run(code) {
+	window.open("http://marvel.com/redeem?code=" + code);
+}
+
+recurse(12, 0, '');
